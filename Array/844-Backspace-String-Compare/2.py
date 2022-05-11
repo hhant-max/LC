9dca6f518b6850bfc1ad374aadcf8ce52a776b38
hhant-max <sofia.sun.apply@outlook.com>
2022-05-11 13:03:28 +0000

    improve by return statement!

---
+++ Array/844-Backspace-String-Compare/2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def remove(s):
+            stk = []
+            for ch in s:
+                if ch=="#":
+                    if stk: stk.pop()
+                else:
+                    stk.append(ch)
+            return stk
+        return remove(s)==remove(t)
