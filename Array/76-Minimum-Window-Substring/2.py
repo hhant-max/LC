789cea5eaeb4cdb71d742ea5691b34ea226289eb
hhant-max <sofia.sun.apply@outlook.com>
2022-05-16 18:59:36 +0000

    final solution

--- Array/76-Minimum-Window-Substring/2.py
+++ Array/76-Minimum-Window-Substring/2.py
@@ -1,27 +1,51 @@
-def  minwindow(s, t) -> str:
+def minWindow(s: str, t: str) -> str:
     countT, window = {},{}
 
     # transfer t into a dict---- key:string value : count number: dictionary get function.
     # 没用数个数的函数二十手动便利每一个， 并且如果存在的话就加到字典并且再原来的基础上加。 如果不存在就返回0
-    for s in t:
-        countT[s] = 1 + countT.get(s,0)
-    
-    countT_number, window_number = len(countT), 0
-    res, reslen = [-1,-1], float("infinity")
-    l = 0
+    for i in t:
+        countT[i] = 1 + countT.get(i,0)
 
+    countT_number, window_number = len(countT), 0
+
+    res, reslen = [-1,-1], float("infinity")
+
+
+    l = 0
     for r in range(len(s)):
         c = s[r]
-        print(f'r is {r}, c is {c}')
 
+        if c in countT.keys():
+            window[c] = 1 + window.get(c,0)
+            
+            # 注意这边的问题是 window【c】不存在怎么办
+            if window[c] == countT[c]:
+                window_number += 1
 
+            while window_number == countT_number:
+                # update the result
+                if (r-l + 1) < reslen:
+                    res = [l,r]
+                    reslen = r-l + 1
+
+                # left pop
+                if s[l] in countT.keys():
+                    #window[s[l]] = -1 + window.get(r)
+                    window[s[l]] -= 1
+                #print(f"s[l] is {s[l]}")
+                #print(f"count_number is {countT_number[s[l]]}")
+                    if window[s[l]] < countT[s[l]]:
+                        window_number -= 1
+
+                l += 1
 
     l, r = res
     return s[l:r+1]
+        
 
 s = "ADOBECODEBANC"
 t = "ABC"
-minwindow(s, t)
+minWindow(s=s, t=t)
 
 #print(len(s))
 #print(s[1])
