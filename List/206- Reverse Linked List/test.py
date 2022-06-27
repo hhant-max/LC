13026e6b21924a3872ff51c9da231739325729fa
hhant-max <sofia.sun.apply@outlook.com>
2022-06-27 16:33:43 +0000

    done

--- List/206- Reverse Linked List/test.py
+++ List/206- Reverse Linked List/test.py
@@ -13,3 +13,20 @@
 '''
 # x宪法先正常写的时候如果到了最后 这个prev。prev无法保障
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        
+        while curr:
+            # 小技巧在于这个 nxt
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+            
