a4406dd6f12131873a699a8dd5f5af37c3455686
hhant-max <sofia.sun.apply@outlook.com>
2022-06-28 15:10:12 +0000

    traverse of string improvement

--- List/242-valid-anagram/test.py
+++ List/242-valid-anagram/test.py
@@ -1,1 +1,18 @@
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        '''
+        test if t and s can trasfer mutually, aka test their appearence occurance.
+        '''
+        
+        # rule out the ocunts clash
+        if len(s) != len(t): return False
+        
+        countS = {}
+        countT = {}
+        
+        # construct hashmap based on string 
+        for i in range(len(s)):
+            countS[i] = 1 + countS.get(i,0)
+            
+        for j in range(len(t)):
+            countT[i] = 1 + countS.get(i,0)
