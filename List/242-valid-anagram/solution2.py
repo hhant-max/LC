fc9afb73bc5f376ca761cc235e698d9df585dfc2
hhant-max <sofia.sun.apply@outlook.com>
2022-06-28 17:44:34 +0000

    solution2

---
+++ List/242-valid-anagram/solution2.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(t) == Counter(s)
+
+    # sort
+    # return sorted(s) == sorted(t)
+
