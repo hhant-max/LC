8b2aa99d7497e7bdfaff25af4b6cedd8d43361b3
hhant-max <sofia.sun.apply@outlook.com>
2022-06-29 12:16:31 +0000

    unknow error

--- List/1-Two-Sum/test.py
+++ List/1-Two-Sum/test.py
@@ -1,12 +1,16 @@
+from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         diff = {}
         
-        for i in range(len(nums)):
-            difference = target - nums[i]
+        for i, n in enumerate(nums):
+            difference = target - n
             # how about the first one??
             # if nums[i] in diff.get(difference,i):
             if difference in diff:
-                return [diff[difference], nums[i]]
+                return [diff[difference], i]
             
             diff[difference] = i 
+                
+s = Solution()
+s.twoSum([1,2,3], target= 3)
