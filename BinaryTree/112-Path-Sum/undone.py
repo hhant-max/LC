30b41a855fa3acbfb568225050a8d15bee2910fd
hhant-max <sofia.sun.apply@outlook.com>
2022-08-24 14:01:51 +0000

    attention no having return value for subfunction

---
+++ BinaryTree/112-Path-Sum/undone.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root: return False
+        
+        def dfs(root,res):
+            if not root.left and not root.right and (res + root.val) == targetSum: return True
+
+            if root.left:
+                res += root.left.val
+                dfs(root.left,res)
+
+            if root.right:
+                res += root.right.val
+                dfs(root.right,res)
+                
+            return 0
+        
+        res = root.val
+        dfs(root,res)
+            
+
+            
+        
